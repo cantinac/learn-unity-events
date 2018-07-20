d951baad5d11f98c3524e2fd7a8e75611ca41461
Scott Doxey <neogeek0101@gmail.com>
2018-07-20 09:40:51 -0400

    Added demo of events in Unity.

---
+++ Assets/Scripts/GroundController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GroundController : MonoBehaviour
+{
+
+    public delegate void EventHandler();
+    public event EventHandler GroundPound;
+
+    public void FireGroundPound()
+    {
+
+        if (GroundPound != null)
+        {
+
+            GroundPound();
+
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/ItemController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+
+    [SerializeField]
+    private LayerMask groundLayerMask;
+
+    [SerializeField]
+    private GroundController groundController;
+
+    private new Rigidbody rigidbody;
+
+    private bool isGrounded;
+
+    private void Awake()
+    {
+
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+
+    }
+
+    private void Update()
+    {
+
+        isGrounded = Physics.CheckSphere(gameObject.transform.position, 0.75f, groundLayerMask);
+
+    }
+
+    private void Bounce()
+    {
+
+        if (isGrounded)
+        {
+
+            rigidbody.AddForce(Vector3.up * 10, ForceMode.Impulse);
+
+        }
+
+    }
+
+    private void OnEnable()
+    {
+
+        groundController.GroundPound += Bounce;
+
+    }
+
+    private void OnDisable()
+    {
+
+        groundController.GroundPound -= Bounce;
+
+    }
+
+    private void OnDrawGizmos()
+    {
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(gameObject.transform.position, 0.75f);
+
+    }
+
+}
